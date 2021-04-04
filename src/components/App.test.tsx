9589737ed85a62468d6841defaaecd241047bb82
Hridayesh Sharma <dev.hridayesh@gmail.com>
2021-04-04 10:14:17 +0530

    changed getAllByPlaceholder by getByPlaceholder

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App', () => {
 	test('renders App component', () => {
 		render(<App />);
 
+		// check if header is in the dom
 		expect(screen.getByText('Github Search')).toBeTruthy();
 
-		const [input] = screen.getAllByPlaceholderText('Enter github username');
+		const input = screen.getByPlaceholderText('Enter github username');
 		expect(input.type).toBe('search');
+		userEvent.type(input, 'vyasriday');
 
 		const button = screen.getByText('Search');
 		expect(button.type).toBe('submit');
-		fireEvent.click(button, {});
+		userEvent.click(button);
+
 		// check if loading screen is shown
 		expect(screen.getByText(/Hang on/)).toBeTruthy();
 	});
